f0d5e6b7a593a79b59f1d737a0bf665526c13c19
AndrewSaffonov <asaffonov@mail.ru>
2023-02-17 01:55:38 +0400

    save version

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,40 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ В методе main класса Main должны происходить следующие операции:
+
+ Создание таблицы User(ов)
+ Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+ Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+ Очистка таблицы User(ов)
+ Удаление таблицы
+ */
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 27);
+        userService.saveUser("Петр", "Петров", (byte) 64);
+        userService.saveUser("Василий", "Васильев", (byte) 39);
+        userService.saveUser("Семен", "Сидоров", (byte) 57);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,121 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+//    private final Connection connection = Util.getConnection();
+    private static final String createUsersTableDdl = "CREATE TABLE IF NOT EXISTS Users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(70), age TINYINT)";
+    private static final String dropUsersTableDdl = "DROP TABLE IF EXISTS Users";
+    private static final String saveUserDml = "INSERT INTO Users (name, lastName,age ) VALUES(?,?,?)";
+    private static final String removeUserByIdDml = "DELETE FROM Users WHERE id = ?";
+    private static final String getAllUsersDql = "SELECT id, name, lastName, age FROM Users";
+//    private static final String getAllUsersDql = "SELECT * FROM Users";
+    private static final String cleanUsersTableDml = "DELETE FROM Users";
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = Util.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(createUsersTableDdl);
+                System.out.println("Таблица создана");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+       } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = Util.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(dropUsersTableDdl);
+                System.out.println("Таблица удалена");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection connection = Util.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement statement = connection.prepareStatement(saveUserDml)) {
+                statement.setString(1, name);
+                statement.setString(2, lastName);
+                statement.setByte(3, age);
+                statement.executeUpdate();
+                System.out.println("User с именем – " + name + " добавлен в базу данных");
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = Util.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement statement = connection.prepareStatement(removeUserByIdDml)) {
+                statement.setLong(1, id);
+                statement.executeUpdate();
+                System.out.println("Удален пользователь ID " + id);
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = Util.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(getAllUsersDql)) {
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getLong("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setLastName(resultSet.getString("lastName"));
+                    user.setAge(resultSet.getByte("age"));
+                    users.add(user);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = Util.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement statement = connection.prepareStatement(cleanUsersTableDml)) {
+                statement.executeUpdate();
+                System.out.println("Таблица очищена");
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
